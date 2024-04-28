878937e49cf0b171e92b3528c48b9855e24e8a55
Mazharul Islam <engrreza4@gmail.com>
2024-04-29 01:59:55 +0600

    music ready

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-const pianoKeys = document.querySelectorAll(".keys .key");
+// const pianoKeys = document.querySelectorAll(".keys .key");
 
-let audio = new Audio("tunes/a.wav");
+// let audio = new Audio("tunes/a.wav");
 
-const playTune = (key) => {
-  audio.play();
-};
-pianoKeys.forEach((key) => {
-  key.addEventListener("click", () => playTune(key.dataset.key));
+// const playTune = (key) => {
+//   audio.play();
+// };
+// pianoKeys.forEach((key) => {
+//   key.addEventListener("click", () => playTune(key.dataset.key));
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const keys = document.querySelectorAll(".key");
+
+  keys.forEach((key) => {
+    const note = key.querySelector("span").innerText;
+    const audio = new Audio(`tunes/${note}.wav`);
+
+    key.addEventListener("click", () => {
+      audio.currentTime = 0; // Reset audio to start if already playing
+      audio.play();
+    });
+  });
 });
